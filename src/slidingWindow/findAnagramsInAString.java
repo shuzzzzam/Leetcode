ffe4709db71654c9637533496dbbcf4c4d3ad9f9
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-11-28 11:11:22 +0530

    Sliding window submission

--- src/slidingWindow/findAnagramsInAString.java
+++ src/slidingWindow/findAnagramsInAString.java
@@ -31,69 +31,40 @@ public class findAnagramsInAString {
         
         HashMap<Character, Integer> map2 = new HashMap<Character, Integer>(map1);
         int count = map2.size();
-        //go in the fixed window size and compare the 
+        //go in the fixed window size and compare the map2
         for(int i = 0; i < p.length(); i++) {
-        	/*if(map2.containsKey(s.charAt(i))) {
-        		int val = map2.get(s.charAt(i));
-        		map2.put(s.charAt(i), val + 1);
-        	}
-        	else {
-        		map2.put(s.charAt(i), 1);
-        	}*/
         	Character ch = s.charAt(i);
-        	if(map2.containsKey(ch) && (map2.get(ch) > 0)) {
+        	if(map2.containsKey(ch)) {
         		int val = map2.get(ch);
-        		if(val > 1) {
-        			map2.put(ch, val - 1);
-        		}
-        		else {
-        			map2.put(ch, 0);
+        		val -= 1;
+        		if(val <= 0) {
         			count--;
         		}
+        		map2.put(ch, val);
         	}
         }
         if(count == 0)
         	list.add(0);
         
         for(int i = 1; i < (s.length() - p.length() + 1); i++) {
-        	/*int val = map2.get(s.charAt(i - 1));
-        	if(val > 1)
-        		map2.put(s.charAt(i - 1), val - 1);
-        	else
-        		map2.remove(s.charAt(i - 1));
-        	
-        	if(map2.containsKey(s.charAt(i + p.length() - 1))) {
-        		val = map2.get(s.charAt(i + p.length() - 1));
-        		map2.put(s.charAt(i + p.length() - 1), val + 1);
-        	}
-        	else {
-        		map2.put(s.charAt(i + p.length() - 1), 1);
-        	}
-        	
-        	if(map2.size() == map1.size())
-        		list.add(i);*/
         	Character ch1 = s.charAt(i - 1);
         	Character ch2 = s.charAt(i + p.length() - 1);
-        	if(map2.containsKey(ch1) && (map2.get(ch1) > 0)) {
+        	if(map2.containsKey(ch1)) {
         		int val = map2.get(ch1);
+        		val += 1;
         		if(val > 0) {
-        			map2.put(ch1, val + 1);
-        		}
-        		else {
-        			map2.put(ch1, 1);
         			count++;
         		}
+        		map2.put(ch1, val);
         	}
         	
-        	if(map2.containsKey(ch2) && (map2.get(ch2) > 0)) {
+        	if(map2.containsKey(ch2)) {
         		int val = map2.get(ch2);
-        		if(val > 1) {
-        			map2.put(ch2, val - 1);
-        		}
-        		else {
-        			map2.put(ch2, 0);
+        		val -= 1;
+        		if(val <= 0) {
         			count--;
         		}
+        		map2.put(ch2, val);
         	}
         	
         	if(count == 0)
