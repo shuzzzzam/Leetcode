5379f899b0f02dc037af33678077272c4d05a3a3
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-03 23:18:21 +0530

    Sliding Window

---
+++ src/slidingWindow/continuousSubarraySum.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package slidingWindow;
+
+/**
+ * @author fSociety
+ *
+ */
+public class continuousSubarraySum {
+	public boolean checkSubarraySum(int[] nums, int k) {
+        boolean flag = false;
+        
+        return flag;
+    }
+}
