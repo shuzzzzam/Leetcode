f747f9d287e131ef8da27703a6c4ebfae3372619
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-03 23:03:48 +0530

    Sliding window & Hashing

--- src/slidingWindow/countSubarraySumOfKInTheArray.java
+++ src/slidingWindow/countSubarraySumOfKInTheArray.java
@@ -14,17 +14,16 @@ public class countSubarraySumOfKInTheArray {
 		/* problem 556(leetcode)
 		 * This question uses variable sliding window techniques to solve.
 		 */
-		
+		//In This question , we will maintain the sum(index i) array for the array value.
 		int[] sum = new int[nums.length + 1];
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int val = 0;
 		int count = 0;
 		sum[0] = 0;
 		for(int i = 1; i <= nums.length; i++) {
-			sum[i] = (val + nums[i - 1]);
+			sum[i] = (sum[i - 1] + nums[i - 1]);
 			
 		}
-		
+		//Now here, this is a sliding window problem converted to simple problem of hashing by evaluating difference, k = S(index b) - S(Index a)
 		for(int i = 0; i <= nums.length; i++) {
 			int searchValueMap = sum[i] - k;
 			if(map.containsKey(searchValueMap))
