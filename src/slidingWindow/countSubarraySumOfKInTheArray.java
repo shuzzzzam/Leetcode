963dfcd5d2dcc9bc46f84a296f271a0afdf86e54
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-03 22:52:20 +0530

    Sliding Window

--- src/slidingWindow/countSubarraySumOfKInTheArray.java
+++ src/slidingWindow/countSubarraySumOfKInTheArray.java
@@ -3,35 +3,41 @@
  */
 package slidingWindow;
 
+import java.util.HashMap;
+
 /**
  * @author fSociety
  *
  */
 public class countSubarraySumOfKInTheArray {
 	public int subarraySum(int[] nums, int k) {
-		/*
+		/* problem 556(leetcode)
 		 * This question uses variable sliding window techniques to solve.
 		 */
 		
-        int count = 0;
-        int head = 0, tail = 0, sum = 0;
-        while((head <= tail) && (tail < nums.length)) {
-        	sum += nums[tail];
-        	if(sum < k) {
-        		tail++;
-        	}
-        	else if(sum == k) {
-        		count++;
-        		sum -= nums[head];
-        		head++;
-        		tail++;
-        		sum += nums[tail];
-        	}
-        	else {
-        		sum -= nums[head];
-        		head++;
-        	}
-        }
-        return count;
+		int[] sum = new int[nums.length + 1];
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int val = 0;
+		int count = 0;
+		sum[0] = 0;
+		for(int i = 1; i <= nums.length; i++) {
+			sum[i] = (val + nums[i - 1]);
+			
+		}
+		
+		for(int i = 0; i <= nums.length; i++) {
+			int searchValueMap = sum[i] - k;
+			if(map.containsKey(searchValueMap))
+				count += (map.get(searchValueMap));
+			if(!map.containsKey(sum[i])) {
+				map.put(sum[i], 1);
+			}
+			else {
+				int tmp = map.get(sum[i]);
+				map.put(sum[i], tmp + 1);
+			}
+		}
+		
+		return count;
     }
 }
