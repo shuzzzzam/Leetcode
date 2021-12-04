5d495cb358ab04d589b1f6fcd34568b37ae157d6
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-04 16:27:00 +0530

    Stack

---
+++ src/stack/NextGreaterElement1.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package stack;
+
+/**
+ * @author fSociety
+ *
+ */
+public class NextGreaterElement1 {
+	public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+		/*
+		 * This problem is solved using stack implementation from right side, since we want next greater from right side
+		 * */
+		
+		int[] hash = new int[10001];
+		for(int i = 0; i < 10001; i++) {
+			hash[i] = -1;
+		}
+		int[] stack = new int[nums2.length];
+		int head = -1;
+		
+		stack[++head] = nums2[nums2.length - 1];
+		hash[nums2.length - 1] = -1;
+		for(int i = (nums2.length - 2); i >= 0; i--) {
+			if(stack[head] > nums2[i]) {
+				hash[nums2[i]] = stack[head];
+				stack[++head] = nums2[i]; 
+			}
+			else {
+				while((head >= 0) && stack[head] < nums2[i]) {
+					head--;
+				}
+				if(head == -1)
+					hash[nums2[i]] = -1;
+				else
+					hash[nums2[i]] = stack[head];
+				
+				stack[++head] = nums2[i];
+			}
+		}
+		int[] res = new int[nums1.length];
+		for(int i = 0; i < nums1.length; i++) {
+			res[i] = hash[nums1[i]];
+		}
+		return res;
+    }
+}
