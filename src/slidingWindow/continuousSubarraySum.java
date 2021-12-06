8a79505b56b79c6685d484d0bd7e2feac3ecfcc5
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-06 23:11:58 +0530

    Sliding window

--- src/slidingWindow/continuousSubarraySum.java
+++ src/slidingWindow/continuousSubarraySum.java
@@ -3,6 +3,8 @@
  */
 package slidingWindow;
 
+import java.util.HashMap;
+
 /**
  * @author fSociety
  *
@@ -10,7 +12,23 @@ package slidingWindow;
 public class continuousSubarraySum {
 	public boolean checkSubarraySum(int[] nums, int k) {
         boolean flag = false;
+        int[] sum = new int[nums.length];
+        sum[0] = (nums[0] % k);
+        //HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] map = new int[k];
+        for(int i = 0; i < map.length; i++)
+        	map[i] = 0;
+        for(int i = 1; i < nums.length; i++) {
+        	sum[i] = (((sum[i - 1] % k) + (nums[i] % k)) % k);
+        }
         
+        for(int i = 2; i <= nums.length; i++) {
+			map[i - 2] = 1;
+			if(map[sum[i]] == 1) {
+				flag = true;
+				break;
+			}
+		}
         return flag;
     }
 }
