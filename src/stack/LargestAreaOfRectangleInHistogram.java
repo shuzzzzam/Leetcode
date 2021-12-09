7d074a49160843f242ea7cadb3f1166792b0ca45
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-09 23:20:14 +0530

    stack

--- src/stack/LargestAreaOfRectangleInHistogram.java
+++ src/stack/LargestAreaOfRectangleInHistogram.java
@@ -10,7 +10,11 @@ package stack;
 public class LargestAreaOfRectangleInHistogram {
 	public int largestRectangleArea(int[] heights) {
         int maxValue = 0;
-        
+        int[] stack = new int[heights.length];
+        int head = -1;
+        for(int i = heights.length - 1; i >= 0; i--) {
+        	
+        }
         return maxValue;
     }
 }
