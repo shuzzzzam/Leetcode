eee00a375035be58b3349014dc6fdd80b793ab9b
shuzzzzam <61275458+shuzzzzam@users.noreply.github.com>
2021-12-10 22:57:33 +0530

    stack & sliding window

--- src/slidingWindow/continuousSubarraySum.java
+++ src/slidingWindow/continuousSubarraySum.java
@@ -10,25 +10,47 @@ import java.util.HashMap;
  *
  */
 public class continuousSubarraySum {
+	/*
+	 * In this problem, we have to use Sliding window technique to find subarray(Atleast 2 elements) whose sum is divisible by k.
+	 * Leetcode problem 523
+	 */
 	public boolean checkSubarraySum(int[] nums, int k) {
         boolean flag = false;
-        int[] sum = new int[nums.length];
-        sum[0] = (nums[0] % k);
-        //HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int[] map = new int[k];
-        for(int i = 0; i < map.length; i++)
-        	map[i] = 0;
+        int[] sum = new int[nums.length]; //Creating a prefix sum array having sum of values indexwise.
+        sum[0] = (nums[0] % k); // Since we want divisible by k, we will use modulo operator.
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(); //map to store till k - 1 value in it. If for the given index value, sum's modulo value is already present, then we know that subarray with given k is present.
         for(int i = 1; i < nums.length; i++) {
-        	sum[i] = (((sum[i - 1] % k) + (nums[i] % k)) % k);
+        	sum[i] = (((sum[i - 1] % k) + (nums[i] % k)) % k); // Good use of modulo operator for divisibility.
         }
-        
-        for(int i = 2; i <= nums.length; i++) {
-			map[i - 2] = 1;
-			if(map[sum[i]] == 1) {
+        map.put(0, 1);
+        for(int i = 1; i < nums.length; i++) {
+            if(i > 1)
+            	map.put(sum[i - 2], 1);
+			if(map.containsKey(sum[i])) {
 				flag = true;
 				break;
 			}
 		}
         return flag;
     }
+	
+	public boolean checkSubarraySumImproved(int[] nums, int k) {
+		boolean flag = false;
+		int i = 0, j = 1, sum = nums[0] % k, lastSum = sum;
+		/*while((i < j) && (j < nums.length)) {
+			sum = (sum + (nums[j] % k)) % k;
+			if(sum == 0) {
+				return true;
+			}
+			else if(sum > lastSum) {
+				j++;
+				lastSum = sum;
+			}
+			else {
+				sum = (sum - (nums[i] % k)) % k;
+			}
+		}*/
+		
+		return flag;
+	}
 }
--- src/slidingWindow/maxSubarray.java
+++ src/slidingWindow/maxSubarray.java
@@ -9,17 +9,21 @@ package slidingWindow;
  */
 public class maxSubarray {
 	public int maxSubArray(int[] nums) {
-        int ans = 0, min = 0;
+        int minSumValue = 0, max = Integer.MIN_VALUE;
         int[] sum = new int[nums.length + 1];
         sum[0] = 0;
         for(int i = 1; i <= nums.length; i++) {
         	sum[i] = (sum[i - 1] + nums[i - 1]);
         }
-        min = sum[1];
-        for(int i = 0; i < nums.length; i++) {
-        	int diff = (nums[i] - min);
+        minSumValue = sum[0];
+        for(int i = 1; i < sum.length; i++) {
+        	int diff = (sum[i] - minSumValue);
+        	if(sum[i] < minSumValue)
+        		minSumValue = sum[i];
+        	if(max < diff)
+        		max = diff;
         }
-        return ans;
+        return max;
         
     }
 }
